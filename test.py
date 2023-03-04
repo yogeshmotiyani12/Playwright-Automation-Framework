c0eeb5523c265cbf712fb1c994f0a3161760e57f
Yogesh Motiyani <49127848+yogeshmotiyani12@users.noreply.github.com>
2023-03-04 23:56:36 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import re
 from playwright.sync_api import Page, expect
 
 
+# Demo Test
+
 def test_homepage_has_Playwright_in_title_and_get_started_link_linking_to_the_intro_page(page: Page):
     page.goto("https://playwright.dev/")
 
